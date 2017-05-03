47dde02b2517a0ead46f045b4af14ec5bf734a64
amydreise-myob <amy.dreise@myob.com>
2017-05-03 14:03:27 +1000

    OCR IS WORKING

--- src/OCRService/triggerv2.js
+++ src/OCRService/triggerv2.js
@@ -1,11 +1,9 @@
 var request = require('request');
-var uuid = require('node-uuid');
 
 module.exports = function(context, req) {
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
     // Make a call out to Cognitive Services
-
         
         request.post({
             url: "https://southeastasia.api.cognitive.microsoft.com/vision/v1.0/ocr",
@@ -18,11 +16,7 @@ module.exports = function(context, req) {
               "url": 'https://help.clover.com/wp-content/uploads/online-receipt-location.jpg'
             }
         }, function(err, res, body) {
-            context.log("Response from Cog API (err, res, body)");
-            context.log(JSON.stringify(err, null, " "));
-            context.log(JSON.stringify(res, null, " "));
-            context.log(JSON.stringify(body, null, " "));
-            
+          
             // Check to see if we succeeded.
             if(err || res.statusCode != 200) {
                 context.log(err);
@@ -37,13 +31,10 @@ module.exports = function(context, req) {
             context.res = {
                 // status: 200, /* Defaults to 200 */
                 // Send back the score we got from the Cog API
-                body: {
-                    score: body.documents[0].score
-                }
+                body
             };
             context.done();
         });
-    }
-
+  
     
 };
