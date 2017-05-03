fcbc6ca31e950e36a9a32e70d0b171b8cfa6ea13
amydreise-myob <amy.dreise@myob.com>
2017-05-03 12:16:19 +1000

    OCR Service Trigger checkpoint

--- src/OCRService/OCRServiceTrigger.js
+++ src/OCRService/OCRServiceTrigger.js
@@ -1,4 +1,34 @@
-module.exports = function (context, myBlob) {
-    context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
-    context.done();
+var http = require('http');
+
+function recogniseTextInImage() {
+    let body = '{"url":"https://help.clover.com/wp-content/uploads/online-receipt-location.jpg"}';
+
+    // An object of options to indicate where to post to
+    var post_options = {
+        url: 'https://southeastasia.api.cognitive.microsoft.com/vision/v1.0/ocr',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': '93eebc96afd14ffba408c8f57af04c91'
+        },
+        json: true,
+        body: {
+            "url": "https://help.clover.com/wp-content/uploads/online-receipt-location.jpg"
+        }
+    };
+    console.log(post_options.body)
+
+    // Set up the request
+    var post_req = http.request(post_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+
+    // post the data
+    // post_req.write(post_data);
+    post_req.end();
 };
+
+recogniseTextInImage();
