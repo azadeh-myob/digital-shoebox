3286aaf2785b81c596ac51c6ee748fdca25b41d8
amydreise-myob <amy.dreise@myob.com>
2017-05-03 13:39:58 +1000

    OCR Service Trigger checkpoint

--- src/OCRService/OCRServiceTrigger.js
+++ src/OCRService/OCRServiceTrigger.js
@@ -1,34 +1,58 @@
-var http = require('http');
+// var http = require('http');
 
 function recogniseTextInImage() {
-    let body = '{"url":"https://help.clover.com/wp-content/uploads/online-receipt-location.jpg"}';
+    let body = {
+            "url": 'https://help.clover.com/wp-content/uploads/online-receipt-location.jpg'
+        };
 
     // An object of options to indicate where to post to
     var post_options = {
-        url: 'https://southeastasia.api.cognitive.microsoft.com/vision/v1.0/ocr',
+        host: 'southeastasia.api.cognitive.microsoft.com',
+        path: '/vision/v1.0/ocr',
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Ocp-Apim-Subscription-Key': '93eebc96afd14ffba408c8f57af04c91'
         },
         json: true,
-        body: {
-            "url": "https://help.clover.com/wp-content/uploads/online-receipt-location.jpg"
-        }
+        body: JSON.stringify(body)
     };
-    console.log(post_options.body)
 
     // Set up the request
-    var post_req = http.request(post_options, function(res) {
-        res.setEncoding('utf8');
+    var post_req = http.request(post_options, function(error, response, body) {
+        // console.log(`STATUS: ${res.statusCode}`);
+        // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+
+        // post_req.on('error', (e) => {
+        //     console.log('error', e);
+        // })
+        // res.on('data', function (chunk) {
+        //     console.log('Response: ' + chunk);
+        // });
+        // res.on('end', () => {
+        //     console.log('No more data in response.');
+        //     post_req.end();
+        // });
+        console.log(error, response, body);
         res.on('data', function (chunk) {
-            console.log('Response: ' + chunk);
+            console.log(error, response, body);
         });
+        res.on('error', function (chunk) {
+            console.log(error, response, body);
+        });
+        if (!error) {
+            //res.write(response.statusCode);
+        } else {
+            //response.end(error);
+            //res.write(error);
+        }
+        res.end(response);
     });
 
     // post the data
-    // post_req.write(post_data);
-    post_req.end();
+    
+    post_req.write(JSON.stringify(body));
+
 };
 
 recogniseTextInImage();
